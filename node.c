afac88b1589e5c372b02abbe00839223b810dd91
Dev_Project <me@projecttl.net>
2021-11-22 15:51:27 +0900

    feat: delete_list, append, insert_after

--- node.c
+++ node.c
@@ -6,12 +6,44 @@ struct NODE {
     int data;
 };
 
+void append(struct NODE **head_node, int new_data) {
+    struct NODE *new_node = (struct NODE *) malloc(sizeof(struct NODE));
+    struct NODE *last = *head_node;
+
+    new_node->data  = new_data;
+    new_node->next = NULL;
+ 
+    if (*head_node == NULL) {
+       *head_node = new_node;
+       return;
+    } 
+
+    while (last->next != NULL)
+        last = last->next;
+  
+    last->next = new_node;
+    return;   
+}
+
 void push(struct NODE **head_node, int new_data) {
     struct NODE *new_node = (struct NODE *)malloc(sizeof(struct NODE));
     new_node->next = (*head_node);
     new_node->data = new_data;
     
     (*head_node) = new_node;
+}
+
+void insert_after(struct NODE *prev_node, int new_data) {
+    if (prev_node == NULL) {
+        printf("The given previous node cannot be NULL");    
+        return;
+    }
+
+    struct NODE *new_node =(struct Node *) malloc(sizeof(struct NODE));
+    
+    new_node->data = new_data;
+    new_node->next = prev_node->next;
+    prev_node->next = new_node;
 }
 
 void delete_node(struct NODE **head_node, int key) {
@@ -40,4 +72,17 @@ void node_list(struct NODE *node) {
         printf(" %d ", node->data);
         node = node->next;
     }
+}
+
+void delete_list(struct NODE** head_node) {
+   struct NODE *current = *head_node;
+   struct NODE *next;
+ 
+   while (current != NULL) {
+       next = current->next;
+       free(current);
+       current = next;
+   }
+
+   *head_node = NULL;
 }
